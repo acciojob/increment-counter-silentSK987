95e696824d026c32c16f7e97fc0bee249e35a5e2
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2023-12-01 00:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn");
+const counter=document.getElementById("counter");
+
+btn.addEventListener("click",(event)=>{
+    event.preventDefault();
+let cnt=parseInt(counter.innerText);
+alert(cnt);
+cnt++;
+counter.innerText=cnt;
+})
